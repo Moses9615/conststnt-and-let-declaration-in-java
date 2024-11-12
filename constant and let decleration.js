cfe07cc2f5006ce0c64823a3d3440c34f106dff1
Moses9615 <146197319+Moses9615@users.noreply.github.com>
2024-11-12 10:14:39 +0300

    Add files via upload

---
+++ constant and let decleration.js
@@ -0,0 +1,14 @@
+const name1 = "Maxwell Wright";
+const phone1 = "0191-7196495";
+const email1 = "Curabitur.egestar.nunc@nonummyac.co.uk";
+
+let name2 = "Raja";
+let phone2 = "08663982895";
+let email2 = "posure.Vulputate@sed.com";
+
+let name3 = "Helen Richard";
+let phone3 = "08001111";
+let email3 = "liboro@convallis.edu";
+console.log(`${name1} / ${phone1} / ${email1}`);
+console.log(`${name2} / ${phone2} / ${email2}`);
+console.log(`${name3} / ${phone3} / ${email3}`);
